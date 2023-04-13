c36ed9c0726e64eb087bdb08f8754dd82bd0e9a9
rigmdata <3092950+smithlabvision@user.noreply.com>
2023-04-13 12:29:44 -0400

    Added updated calibrate function for internalStateAxisDecoder

--- ex_bci/bciCalibration/calibrateInternalStateAxisDecoder.m
+++ ex_bci/bciCalibration/calibrateInternalStateAxisDecoder.m
@@ -18,6 +18,9 @@ end
 nevFilesForTrain(strcmp(nevFilesForTrain, nevFilebase)) = [];
 
 %% Read in NEV files that will be used for training our decoder
+% Base usually set to the first file of training Nevs
+% TODO: Figure out what this nevFileBase actually does ( Maybe used to
+% determine timepoints w.r.t other Nev files)
 [nevBase,waves] = readNEV(nevFilebase);
 nev = nevBase;
 kr = [];
@@ -38,10 +41,10 @@ for nevFlInd = 1:length(nevFilesForTrain)
     nev = [nev; nevNx];
     waves = [waves, wavesNx];
 end
-% Convert Nevs to dat so that its easier to work with in Matlab
-datBase = nev2dat(nevBase, 'nevreadflag', true);
 % Run NASNET on NEVs
 [~,nevLabelledData] = runNASNet({nev, waves},gamma, 'netFolder', netFolder, 'netname', nasNetName, 'labelSpikesAsWithWrite', true);
+% Convert Nevs to dat so that its easier to work with in Matlab
+datStruct = nev2dat(nevLabelledData, 'nevreadflag', true);
 %% Initialize BCI-specific parameters
 % Setup filepaths to where BCI decoder will be saved.
 bciDecoderSaveDrive = params.bciDecoderBasePathDataComputer;
@@ -55,54 +58,112 @@ if ~success
 end
 samplingRate = params.neuralRecordingSamplingFrequencyHz; % samples/s
 binSizeMs = trainParams.binSizeMs; % ms
+% TODO: Figure these out
 % Parameters used to identify valid channels that will be used for BCI.
 frThresh = trainParams.firingRateThreshold;
 ffThresh = trainParams.fanoFactorThreshold;
 coincThresh = trainParams.coincThresh;
 coincTimeMs = trainParams.coincTimeMs;
-timeDecoderDelay = 0; % ms;
-binDecoderDelay = timeDecoderDelay/binSizeMs;
 %% Preprocess and train decoder using calibration data
 % remove all non-calibration trials, as they don't have correct spiking
-% data and will mess up identification of internal state axi
-calibrationTrialCode = codes.BACKGROUND_PROCESS_TRIAL;
-calibrationTrialsInds = cellfun(@(x) any(ismember(x(:, 2), calibrationTrialCode)), {datStruct.event});
+% data and will mess up identification of internal state axis
+nonCalibrationTrialCode = codes.BACKGROUND_PROCESS_TRIAL; % Code 250 should refer to nonCalibration Trials
+nonCalibrationTrialsInds = cellfun(@(x) any(ismember(x(:, 2), nonCalibrationTrialCode)), {datStruct.event});
 
+% Preprocess data according to criteria we apply (FR threshold, coincidence
+% detection, etc.)
 [trimmedDat, channelsKeep] = preprocessDat(datStruct, nevLabelledData, channelNumbersUse, binSizeMs, frThresh, ffThresh, coincThresh, coincTimeMs);
-trimmedDat = trimmedDat(~calibrationTrialsInds);
+% Dat should only contain calibration trials
+trimmedDat = trimmedDat(~nonCalibrationTrialsInds);
 fprintf("\n%d channels being used\n", length(channelsKeep));
 
 % For neural signals, look at delay period right before go cue is sent
 % during calibration trials
-% find region that is 250ms before the binEnd code
-codeForBinEnd = codes.(trainParams.binEndCode);
+% TODO: Specify what this calBinEndCode will be for our task; it's currently
+% used here to determine the end of our bins that we'll use for training
+codeForBinEnd = codes.(trainParams.calBinEndCode); 
 
 % Train only on trials that have matching trainingResultCodes
+% TODO: Specify what trainingResultCodes we will use in our task
+if iscell(trainParams.trainingResultCodes)
+    resultCodesForTrialsToKeep = cellfun(@(resCode) codes.(resCode),  trainParams.trainingResultCodes);
+elseif ischar(trainParams.trainingResultCodes)
+    resultCodesForTrialsToKeep = codes.(trainParams.trainingResultCodes);
+end
 
-% Only look at the fixation periods of trainTrials; this is because the
-% number of trials with fixate codes and number of fixation_off codes aren't equal. 
-[fixStartIndex, ~] = cellfun(@(x) find(x(:, 2)==codeForBinStart), {trimmedDat.event}, 'uni', 0);
-[fixEndIndex, ~] = cellfun(@(x) find(x(:, 2)==codeForBinEnd), {trimmedDat.event}, 'uni', 0);
+% Identify calibration trial indices that have matching result codes
+trainTrials = cellfun(@(x) any(ismember(x(:, 2), resultCodesForTrialsToKeep)), {trimmedDat.event});
 
+% Only look at the delay periods of trainTrials; this will be timeBeforeEndCode ms before
+% the calBinEndCode that we provide
+% Identify samples that correspond to the end and beginning for epoch
+% Go back 250ms from end Sample 
+% TODO: Is this the time period we want before the epoch?
+timeFromEndOfEpoch = 250; %in ms
+samplingRate = params.neuralRecordingSamplingFrequencyHz; % samples/s
+% First element of vector in cellfun is the start sample for delay epoch
+% and second sample is the end of that epoch
+[epochStartEndSampleIndices] = cellfun(@(x) [x(x(:, 2)==codeForBinEnd, 3) - samplingRate*timeFromEndOfEpoch/1000, x(x(:, 2)==codeForBinEnd, 3)] , {trimmedDat.event}, 'uni', 0);
 % Get all spike times and spike channels from dat
 spikeTimes = cellfun(@(firstSpike, spikeTimeDiffs) [firstSpike; firstSpike+cumsum(uint64(spikeTimeDiffs))], {trimmedDat.firstspike}, {trimmedDat.spiketimesdiff}, 'uni', 0);
 spikeChannels = cellfun(@(spikeInfo) spikeInfo(:, 1), {trimmedDat.spikeinfo}, 'uni', 0);
-%% Training the Motivation Axis Decoder
-% grab the neural activity during the delay epochs of all trials that we
-% wish to train on
-delayValidTrials = ~cellfun('isempty', fixEndIndex) & ~cellfun('isempty', fixStartIndex) & trainTrials;
+%% Grab Neural activity that will be used to fit the internal state axis 
+% grab the neural activity during the delay epochs for training trials
+% All trials that have an epoch end are valid trials to train on
+binSizeMs = trainParams.binSizeMs; % ms
+msPerS = 1000; % 1000 ms in a second
+binSizeSamples = binSizeMs/msPerS*samplingRate;
+delayValidTrials = ~cellfun('isempty', epochStartEndSampleIndices) & trainTrials;
+% Initialize cell that will store delay epoch spikes
 binnedSpikesDelay = cell(size(spikeTimes));
 % Bin spikes involved in delay period
 binnedSpikesDelay(delayValidTrials) = cellfun(...
-        @(trialSpikeTimes, chanNums, fixStartEndTime)... all the inputs
+        @(trialSpikeTimes, chanNums, trialEpochStartEndTime)... all the inputs
         ... adding binSizeSamples/2 ensures that the last point is used
         ... while subtracting 0.1 is for a super corner case where a spike overlaps the last bin end
         ... --histcounts2 includes this in the last bin (unlike what it does for every other bin);
         ... as spike times are integers the 0.1 subtraction changes no other bins
-        histcounts2(trialSpikeTimes, chanNums, (fixStartEndTime(1):binSizeSamples:fixStartEndTime(2)+binSizeSamples/2)-0.1, 1:max(channelsKeep)+1),... histcounts2 bins things in 50ms bins and groups by channel
-        spikeTimes(delayValidTrials), spikeChannels(delayValidTrials), fixStartEndTimes(delayValidTrials), 'uni', 0);
+        histcounts2(trialSpikeTimes, chanNums, (trialEpochStartEndTime(1):binSizeSamples:trialEpochStartEndTime(2)+binSizeSamples/2)-0.1, 1:max(channelsKeep)+1),... histcounts2 bins things in 50ms bins and groups by channel
+        spikeTimes(delayValidTrials), spikeChannels(delayValidTrials), epochStartEndSampleIndices(delayValidTrials), 'uni', 0);
 % Only keep binned counts of channels that are kept
 binnedSpikesDelay(delayValidTrials) = cellfun(@(bS) bS(:, channelsKeep), binnedSpikesDelay(delayValidTrials), 'uni', 0);
-allBinnedCountsFixationValidTrials = binnedSpikesDelay(delayValidTrials)';
-
+allTrialsDelayEpochBinnedCounts = binnedSpikesDelay(delayValidTrials)';
+% TODO: Do we take the average of the bins for the actual BCI? How do we use these bins?
+%% Fit FA to binned counts to help denoise 
+% Concatenate all trials binned spikes
+binnedSpikesAllConcat = cat(1, allTrialsDelayEpochBinnedCounts{:});
+% TODO: Figure out the number of latents necessary
+numLatents = trainParams.numberFaLatents; % this is how many latents we'll project to, no matter what...
+if trainParams.zScoreSpikes
+    zScoreSpikesMat = diag(1./std(binnedSpikesAllConcat, [], 1));
+    % Subtract the mean spike count vector and then divide by the variance
+    % of spikes
+    binnedSpikesAllConcat = (binnedSpikesAllConcat - mean(binnedSpikesAllConcat)) * zScoreSpikesMat;
+else
+    % Do nothing
+end
+% Identify FA Space using ALL bins of delay period 
+% (num_bins_in_delay* num_trials x num_channels)
+[estParams, ~] = fastfa(binnedSpikesAllConcat', numLatents);
+% Beta is simply the projection matrix into the factor space
+[~, ~, beta] = fastfa_estep(binnedSpikesAllConcat', estParams);
+%% Fit LDA to these FA Projections
+% Keep trial parameters of valid trials that will be used for training LDA
+delayValidTrialParams = trimmedDat(delayValidTrials);
+numValidTrials = length(delayValidTrialParams);
+validTrialRewardLabels = nan(numValidTrials, 1);
+for k = 1:numValidTrials
+    validTrialRewardLabels(k) = delayValidTrialParams(k).params.trial.variableRewardIdx;
+end
+% Take Mean binned activity during delay period then project into FA space
+meanDelayValidTrialFAProjs = cellfun(@(x) beta*(mean(x)' - estParams.d), allTrialsDelayEpochBinnedCounts, 'UniformOutput', false);
+% Concatenate across all trials to get a num_valid_trials x num_fa_latents
+ldaTrainX = cat(2, meanDelayValidTrialFAProjs{:})';
+% Fit LDA on FA projections
+ldaParams = fit_LDA(ldaTrainX, validTrialRewardLabels);
+%% Save model parameters 
+bciDecoderSaveName = sprintf('rewardAxisDecoder_%s.mat', datestr(now, 'yyyy-mm-dd_HH-MM-SS'));
+save(fullfile(bciDecoderSaveFolder, bciDecoderSaveName), 'ldaParams', 'beta','channelsKeep', 'nevFilebase', 'nevFilesForTrain', 'includeBaseForTrain', 'nasNetName');
+decoderFileLocationAndName = fullfile(bciDecoderRelativeSaveFolder, bciDecoderSaveName);
+fprintf('decoder file saved at : %s\n', decoderFileLocationAndName)
 end
