89b405300a72b1acc799b1f7a9b560289af6313f
smithlabrig <get>
2023-04-18 15:32:51 -0400

    Add updated rewardAxisDecodr

--- ex_bci/decoders/rewardAxisDecoder.m
+++ ex_bci/decoders/rewardAxisDecoder.m
@@ -11,22 +11,29 @@ ldaParams = modelParams.ldaParams;
 beta = modelParams.beta; % Will be projection matrix to project values into FA space
 estFAParams = modelParams.estFAParams;
 zScoreSpikesMat = modelParams.zScoreSpikesMat;
+zScoreSpikesMuTerm = modelParams.zScoreSpikesMuTerm;
 d = modelParams.estFAParams.d; % mean spike count vector during calibration trials (useful for z-scoring too)
 
 % Exponential Smoothing parameters
 alpha = expParams.alpha;
 
-% Project zscored spikes into  FA space
-% TODO: Should we subtract the mean spike count vector
-currFAProjs = beta * ((zScoreSpikesMat * meanSpikeCount)  - d);
-% is d = mean or mean/std? if it's just the mean then: 
-% currFAProjs = beta * ((zScoreSpikesMat * (meanSpikeCount  - d));
-
+% Z-score spikes; zscoreSpikesMat will be identity and zScoreSpikesMuTerm
+% will be zero if zscoreSpikes is set to false in trainParams
+zScoredSpikes = (zScoreSpikesMat * meanSpikeCount) - zScoreSpikesMuTerm;
+% If FA is fitted on zScoredSpikes, d will be zero. Else, make sure to
+% subtract mean.
+currFAProjs = beta * (zScoredSpikes - d);
 % Compute LDA Projection of FA Projs
 rewardAxisProj = currFAProjs*ldaParams.projVec;
 
 % Compute how far projection is from requested internal state
-% Need to compute distance 
+% Setting small to 1 and Large to anything else
+% Check if requested reward state is small else assume it is large
+if requestedRewardState == 1
+    
+else
+end
+    % Need to compute distance 
 rewardAxisDistance = 1;
 
 % Compute smoothed distance values
