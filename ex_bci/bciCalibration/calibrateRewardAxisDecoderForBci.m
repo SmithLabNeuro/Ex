819b2e520c57905c6118e4adefe0a66caee34588
rigmdata <125372272+smithlabrig@users.noreply.github.com>
2023-06-29 14:23:58 -0400

    Added helper function to flip axes based on condition. Also, removed storage of SD for reward decoder

--- ex_bci/bciCalibration/calibrateRewardAxisDecoderForBci.m
+++ ex_bci/bciCalibration/calibrateRewardAxisDecoderForBci.m
@@ -200,28 +200,7 @@ ldaParams = fit_LDA(ldaTrainX, validTrialRewardRepeatedLabels);
 % Keep track of Small/large 
 smallRewardProjs = ldaParams.projData(find(validTrialRewardRepeatedLabels == 1));
 largeRewardProjs = ldaParams.projData(find(validTrialRewardRepeatedLabels == 3));
-% SDs will be used to find new target
-largeRewardSD = std(largeRewardProjs);
-smallRewardSD = std(smallRewardProjs);
-% Have targets be defined
-largeRewardTarget = mean(largeRewardProjs);
-smallRewardTarget= mean(smallRewardProjs);
-% Flip reward axis only if smallReward projection is higher than large reward projection
-if smallRewardTarget > largeRewardTarget
-    ldaParams.projVec = ldaParams.projVec*-1;
-    ldaParams.projData = ldaParams.projData*-1;
-    smallRewardProjs = -smallRewardProjs;
-    largeRewardProjs = -largeRewardProjs;
-    largeRewardTarget = -largeRewardTarget;
-    smallRewardTarget = -smallRewardTarget;
-end
-
-% Have targets be pushed up/down by SDs
-largeRewardTarget = mean(largeRewardProjs) + trainParams.targChangeByStd*largeRewardSD;
-smallRewardTarget= mean(smallRewardProjs) - trainParams.targChangeByStd*smallRewardSD;
-% Set different R values for the two conditions 
-largeRewardRange = largeRewardTarget - prctile(smallRewardProjs, 10);
-smallRewardRange = prctile(largeRewardProjs, 90) - smallRewardTarget;
+[smallRewardProjs, largeRewardProjs, smallRewardTarget, largeRewardTarget, smallRewardRange,largeRewardRange, ldaParams] = flipAxesBasedOnCondition(smallRewardProjs, largeRewardProjs, ldaParams, trainParams.targChangeByStd);
 %% Generate Projections along reward Axis
 figure;
 hold on
@@ -251,7 +230,7 @@ else
     bciDecoderSaveName = sprintf('%s%sRewardAxisBci_%s.mat', subjectCamelCase(1:2), datestr(today, 'yymmdd'), datestr(now, 'HH-MM-SS'));
 end
 
-save(fullfile(bciDecoderSaveFolder, bciDecoderSaveName), 'ldaParams', 'estFAParams', 'beta', 'zScoreSpikesMat', 'zScoreSpikesMuTerm', 'channelsKeep', 'nevFilebase', 'nevFilesForTrain', 'includeBaseForTrain', 'nasNetName', 'largeRewardTarget', 'smallRewardTarget', 'largeRewardRange', 'smallRewardRange', 'largeRewardSD', 'smallRewardSD', 'trainParams', 'initialSeedValue');
+save(fullfile(bciDecoderSaveFolder, bciDecoderSaveName), 'ldaParams', 'estFAParams', 'beta', 'zScoreSpikesMat', 'zScoreSpikesMuTerm', 'channelsKeep', 'nevFilebase', 'nevFilesForTrain', 'includeBaseForTrain', 'nasNetName', 'largeRewardTarget', 'smallRewardTarget', 'largeRewardRange', 'smallRewardRange', 'trainParams', 'initialSeedValue');
 decoderFileLocationAndName = fullfile(bciDecoderRelativeSaveFolder, bciDecoderSaveName);
 fprintf('decoder file saved at : %s\n', decoderFileLocationAndName)
 end
---
+++ ex_bci/bciCalibration/utils/flipAxesBasedOnCondition.m
@@ -0,0 +1,21 @@
+function [condOneProjs, condTwoProjs, condOneTarget, condTwoTarget, condOneRange,condTwoRange, ldaParams] = flipAxesBasedOnCondition(condOneProjs, condTwoProjs,ldaParams, targChangeByStd)
+    % SDs will be used to find new target
+    condTwoProjsSD = std(condTwoProjs);
+    condOneProjsSD = std(condOneProjs);
+    % Have targets be defined
+    condTwoTarget = mean(condTwoProjs);
+    condOneTarget= mean(condOneProjs);
+    % Flip axis only if condTwo projection is higher than condTwo projection
+    if condOneTarget > condTwoTarget
+        ldaParams.projVec = ldaParams.projVec*-1;
+        ldaParams.projData = ldaParams.projData*-1;
+        condOneProjs = -condOneProjs;
+        condTwoProjs = -condTwoProjs;
+    end
+    % Have targets be pushed up/down by SDs
+    condTwoTarget = mean(condTwoProjs) + targChangeByStd*condTwoProjsSD;
+    condOneTarget= mean(condOneProjs) - targChangeByStd*condOneProjsSD;
+    % Set different R values for the two conditions 
+    condTwoRange = condTwoTarget - prctile(condOneProjs, 10);
+    condOneRange = prctile(condTwoProjs, 90) - condOneTarget;
+end
