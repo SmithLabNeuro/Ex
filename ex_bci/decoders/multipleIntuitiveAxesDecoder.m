088cc0d2208412730f9ca02105cfe88e6b9f96b1
rigmbci <30929050+smithlabvision@user.noreply.github.com>
2023-07-14 11:59:59 -0400

    Update multiple intuitive axes decoding

--- ex_bci/decoders/multipleIntuitiveAxesDecoder.m
+++ ex_bci/decoders/multipleIntuitiveAxesDecoder.m
@@ -3,18 +3,16 @@ function newReturn = multipleIntuitiveAxesDecoder(meanSpikeCount, currReturn, mo
 % vector
 % modelParams - info saved by calibration function
 % expParams - from bci_rewardAxisDecoder.xml
+persistent currSmoothedLDAProjs
 
-numFaLatents = expParams.numberFaLatents;
-persistent currSmoothedFAProjs
+currTargetState = currReturn(3);
+currAxisToUse = currReturn(4);
 
-if isempty(currSmoothedFAProjs)
-    currSmoothedFAProjs = zeros(numFaLatents,1);
+% Start of trial, use the initial seeded values here
+if isempty(currSmoothedLDAProjs)
+    currSmoothedLDAProjs = modelParams.initialSeedValues(currAxisToUse,currTargetState);
 end
 
-currDistToTarget = currReturn(1);
-currAnnulusRad = currReturn(2);
-currRewardState = currReturn(3);
-currAxisToUse = currReturn(4);
 % Grab decoder parameters 
 multipleAxesLDAParams = modelParams.multipleAxesLDAParams; % num_axes x 1 struct array that contains LDA params for each axis trained in calibration
 beta = modelParams.beta; % Will be projection matrix to project values into FA space, 10 x neurons
@@ -28,15 +26,9 @@ zScoreSpikesMuTerm = modelParams.zScoreSpikesMuTerm;
 % Exponential Smoothing parameters
 alpha = expParams.alpha;
 
-% Get the targets and ranges for all axes  
-lowHighTargetStates = modelParams.lowHighTargetStates;
-lowHighTargetRanges = modelParams.lowHighTargetRanges;
-
 % Determine relevant parameters for mapping being currently used
-condTwoTarget = modelParams.lowHighTargetStates(currAxisToUse,2);
 condOneTarget = modelParams.lowHighTargetStates(currAxisToUse,1);
-condTwoRange = modelParams.lowHighTargetRanges(currAxisToUse,2);
-condOneRange = modelParams.lowHighTargetRanges(currAxisToUse,1);
+condTwoTarget = modelParams.lowHighTargetStates(currAxisToUse,2);
 currLDAParams = multipleAxesLDAParams(currAxisToUse);
 
 % Z-score spikes; zscoreSpikesMat will be identity and zScoreSpikesMuTerm
@@ -46,23 +38,26 @@ zScoredSpikes = (zScoreSpikesMat * meanSpikeCount) - zScoreSpikesMuTerm;
 % subtract mean.
 newFaProjs = beta * (zScoredSpikes - d);
 
-% apply exponential smoother to FA projections
-newSmoothFaProj = (1-alpha) .* currSmoothedFAProjs + alpha .* newFaProjs;
+% Compute LDA Projection of FA Projs 
+currLDAProjection = currLDAParams.projVec' * newFaProjs;
 
-% Compute LDA Projection of smoothed FA Projs
-oneDimAxisProj = currLDAParams.projVec' * newSmoothFaProj; % 1 x 1?
+% Smooth LDA projection 
+currSmoothedLDAProjs = (1-alpha) .* currSmoothedLDAProjs + alpha*(currLDAProjection); % 1 x 1?
+
+% Determine the range to use based on the current target state
+oneDimAxisRange = modelParams.lowHighTargetRanges(currAxisToUse,currTargetState);
 
 % Compute how far projection is from requested internal state
 % Setting small to 1 and Large to anything else
 % Check if requested reward state is small else assume it is large
-if currRewardState == 1
+if currTargetState == 1
+    % low
     % should be negative when rewardAxisProj is below smallReward Target
-    newDistToTarget = oneDimAxisProj - condOneTarget;
-    oneDimAxisRange = condOneRange;
+    newDistToTarget = currSmoothedLDAProjs - condOneTarget;
 else
+    % high
     % should be negative when rewardAxisProjs is above largeReward Target
-    newDistToTarget = condTwoTarget - oneDimAxisProj;
-    oneDimAxisRange = condTwoRange;
+    newDistToTarget = condTwoTarget - currSmoothedLDAProjs;
 end
 
 % Compute distance ratio that will be used for annulus value calculation
@@ -79,9 +74,6 @@ if oneDimAxisRatio < 0
     oneDimAxisRatio = 0;
 end
 
-% Set new smoothed fa projs as curr fa Projs
-currSmoothedFAProjs = newSmoothFaProj;
-
 % Set newSmoothedDist for return
 newAnnulusRad = round(oneDimAxisRatio * expParams.maxAnnulusRad);
-newReturn = [newDistToTarget; newAnnulusRad; currRewardState, currAxisToUse];
+newReturn = [newDistToTarget; newAnnulusRad; currTargetState; currAxisToUse];
