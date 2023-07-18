521b3eb3a02a58200ecf6c336311fa66876d25fe
rigmdata <125372272+smithlabrig@users.noreply.github.com>
2023-07-18 13:37:31 -0400

    Updated flipAxesBasedOnCondition to only flip and get projections

--- ex_bci/bciCalibration/utils/flipAxesBasedOnCondition.m
+++ ex_bci/bciCalibration/utils/flipAxesBasedOnCondition.m
@@ -1,25 +1,17 @@
-function [condOneProjs, condTwoProjs, condOneTarget, condTwoTarget, condOneRange,condTwoRange, ldaParams] = flipAxesBasedOnCondition(condOneProjs, condTwoProjs,ldaParams, targChangeByStd)
-    % SDs will be used to find new target
-    condTwoProjsSD = std(condTwoProjs);
-    condOneProjsSD = std(condOneProjs);
-    % Have targets be defined
-    condTwoTarget = mean(condTwoProjs);
-    condOneTarget= mean(condOneProjs);
+function [condOneProjs, condTwoProjs, condOneMean, condTwoMean, condOneProjsSD,condTwoProjsSD, ldaParams] = flipAxesBasedOnCondition(condOneProjIndices, condTwoProjIndices, ldaParams)
     % Flip axis only if condTwo projection is higher than condTwo projection
-    if condOneTarget > condTwoTarget
+    condOneProjs = ldaParams.projData(condOneProjIndices);
+    condTwoProjs = ldaParams.projData(condTwoProjIndices);
+    if mean(condOneProjs) > mean(condTwoProjs)
         ldaParams.projVec = ldaParams.projVec*-1;
         ldaParams.projData = ldaParams.projData*-1;
         condOneProjs = -condOneProjs;
         condTwoProjs = -condTwoProjs;
     end
-    % Have targets be pushed up/down by SDs
-    condTwoTarget = mean(condTwoProjs) + targChangeByStd*condTwoProjsSD;
-    condOneTarget= mean(condOneProjs) - targChangeByStd*condOneProjsSD;
-    fprintf('Target One is %d\n', condOneTarget)
-    fprintf('Target Two is %d\n', condTwoTarget)
-    % Set different R values for the two conditions 
-    condTwoRange = condTwoTarget - prctile(condOneProjs, 1);
-    condOneRange = prctile(condTwoProjs, 99) - condOneTarget;
-    fprintf('Range for cond One is %d\n', condOneRange)
-    fprintf('Range for cond Two is %d\n', condTwoRange)
+    % SDs will be used to find new target
+    condTwoProjsSD = std(condTwoProjs);
+    condOneProjsSD = std(condOneProjs);
+    % Have targets be defined
+    condOneMean= mean(condOneProjs);
+    condTwoMean = mean(condTwoProjs);
 end
