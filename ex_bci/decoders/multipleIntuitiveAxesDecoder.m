d68e208e86250e0d87bfa72befac333f8e434e02
rigmbci <30929050+smithlabvision@user.noreply.github.com>
2023-07-18 13:29:35 -0400

    Update bci decoder code for intuitive Axes

--- ex_bci/decoders/multipleIntuitiveAxesDecoder.m
+++ ex_bci/decoders/multipleIntuitiveAxesDecoder.m
@@ -10,9 +10,31 @@ persistent currSmoothedLDAProjs
 currTargetState = currReturn(3);
 currAxisToUse = currReturn(4);
 
+currMappingTargetParams = modelParams.mappingTargetParams{currAxisToUse}(currTargetState);
+
+% Determine percentile that will be used for range based on target state
+oppositePercentileForRange = expParams.oppositePercToUseForCondOne;
+if currTargetState == 2
+    oppositePercentileForRange= 100 - oppositePercentileForRange;
+end
+oppositeTargState = mod(currTargetState,2) + 1;
+oppositeTargExtremePerc = prctile(modelParams.mappingTargetParams{currAxisToUse}(oppositeTargState).LDAProjs, oppositePercentileForRange);
+
 % Start of trial set initial seed value for LDA projection
 if isempty(currSmoothedLDAProjs)
-    currSmoothedLDAProjs = modelParams.initialSeedValues(currAxisToUse, currTargetState);
+    % Seed initial value using percentile from opposite mapping
+    currSmoothedLDAProjs = oppositeTargExtremePerc;
+end
+targsChangeBySD = expParams.targChangeByStd;
+
+
+% initialize the range and target
+if currTargetState == 1
+    currTargRange = oppositeTargExtremePerc - currMappingTargetParams.mean;
+    currTargReqState = currMappingTargetParams.mean - currMappingTargetParams.std*targsChangeBySD;
+else
+    currTargRange = currMappingTargetParams.mean - oppositeTargExtremePerc;
+    currTargReqState = currMappingTargetParams.mean + currMappingTargetParams.std*targsChangeBySD;
 end
 
 % Grab decoder parameters 
@@ -21,16 +43,13 @@ beta = modelParams.beta; % Will be projection matrix to project values into FA s
 estFAParams = modelParams.estFAParams;
 d = estFAParams.d; % mean spike count vector during calibration trials (useful for z-scoring too)
 
-% Zscoring parameters
+% Zscoring parametersmodelParams.initialSeedValues(currAxisToUse, currTargetState);
 zScoreSpikesMat = modelParams.zScoreSpikesMat;
 zScoreSpikesMuTerm = modelParams.zScoreSpikesMuTerm;
 
 % Exponential Smoothing parameters
 alpha = expParams.alpha;
 
-% Determine relevant parameters for mapping being currently used
-condOneTarget = modelParams.lowHighTargetStates(currAxisToUse,1);
-condTwoTarget = modelParams.lowHighTargetStates(currAxisToUse,2);
 currLDAParams = multipleAxesLDAParams(currAxisToUse);
 
 % Z-score spikes; zscoreSpikesMat will be identity and zScoreSpikesMuTerm
@@ -44,25 +63,22 @@ currLDAProjs = currLDAParams.projVec'*newFaProjs;
 % apply exponential smoother to LDA projections
 currSmoothedLDAProjs = (1-alpha)*currSmoothedLDAProjs + alpha*currLDAProjs;
 
-% Determine the range to use based on the current target state
-oneDimAxisRange = modelParams.lowHighTargetRanges(currAxisToUse,currTargetState);
-
 % Compute how far projection is from requested internal state
 % Setting small to 1 and Large to anything else
 % Check if requested reward state is small else assume it is large
 if currTargetState == 1
     % low
     % should be negative rewardAxisProjwhen rewardAxisProj is below smallReward Target
-    newDistToTarget = currSmoothedLDAProjs - condOneTarget;
+    newDistToTarget = currSmoothedLDAProjs - currTargReqState;
 else
     % high
     % should be negative when rewardAxisProjs is above largeReward Target
-    newDistToTarget = condTwoTarget - currSmoothedLDAProjs;
+    newDistToTarget = currTargReqState - currSmoothedLDAProjs;
 end
 
 % Compute distance ratio that will be used for annulus value calculation
 % (r/R) in schematic
-oneDimAxisRatio = newDistToTarget/oneDimAxisRange;
+oneDimAxisRatio = newDistToTarget/currTargRange;
 
 % Need to saturate annulus values like PFC BCI
 if oneDimAxisRatio > 1
