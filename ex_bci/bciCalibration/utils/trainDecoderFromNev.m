43294df72eb8e50e4758d364326a9b005cdc5759
rigmdata <125372272+smithlabrig@users.noreply.github.com>
2023-08-31 16:28:32 -0400

    Update decoder to take in xml file as input

--- ex_bci/bciCalibration/utils/trainDecoderFromNev.m
+++ ex_bci/bciCalibration/utils/trainDecoderFromNev.m
@@ -1,10 +1,10 @@
-function [decoderFileLocationAndName] = trainDecoderFromNev(nevFilebase, subject)
+function [decoderFileLocationAndName] = trainDecoderFromNev(nevFilebase, xmlFile, subject)
 % function [M0, M1, M2, channelsKeep, A, Q, C, R, beta, K] = trainKalmanDecoderFromNev(nev, trainParams, params, codes, datBase, nevBase,...
 %         netLabels, gamma, channelNumbersUse, Qvalue, numLatents, velocityToCalibrateWith, includeBaseForTrain)
 
 exGlobals;
 dataPath = 'E:\';
-xmlFile = 'bci_rewardAxisDecoder.xml';
+
 [~,machineInit] = system('hostname');
 machine = lower(deblank(cell2mat(regexp(machineInit, '^[^\.]+', 'match'))));
 
@@ -12,4 +12,4 @@ nevFilename = fullfile(dataPath, nevFilebase);
 [~, trainParams, ~, ~] = readExperiment(xmlFile,subject,machine);
 
 % In this context, nevFileBase matches nevFilesForTrain 
-decoderFileLocationAndName = calibrateRewardAxisDecoderForBci('', nevFilename, {nevFilename}, trainParams, subject, true);
+decoderFileLocationAndName = calibrateMultipleAxisDecoderForBci('', nevFilename, {nevFilename}, trainParams, subject, true);
