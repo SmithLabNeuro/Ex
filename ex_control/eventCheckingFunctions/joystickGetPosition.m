91a005294154df69ccba701181c3d71ebf0bb9e1
rigmdata <125372272+smithlabrig@users.noreply.github.com>
2023-09-15 12:43:22 -0400

    remove event checkers

--- ex_control/eventCheckingFunctions/joystickGetPosition.m
+++
@@ -1,9 +0,0 @@
-function success = joystickGetPosition(~, ~, analogChannelPosInput)
-
-posOut = zeros(size(analogChannelPosInput));
-for channel = 1:length(analogChannelPosInput)
-    chPos = unixGetAnalogInput(analogChannelPosInput);
-    posOut(channel) = chPos;
-end
-
-success = posOut;
