22359cb9c8f20d123bd29bc634a9bc1201416d7b
rigmdata <125372272+smithlabrig@user.noreply.github.com>
2023-10-31 14:59:25 -0400

    Update exGlobals.m

--- ex_control/exGlobals.m
+++ ex_control/exGlobals.m
@@ -114,6 +114,7 @@ codes = struct();
 
 % trial boundaries
 codes.START_TRIAL = 1;
+codes.BCI_END = 249; % Use to indicate that this is end the BCI component of a trial. Useful when trial contains bci component and non-bci task (ie, reaching).
 codes.BACKGROUND_PROCESS_TRIAL = 250; % use to indicate this is a 'special' trial whose timing/results should not be analyzed--i.e. training a BCI decoder
 codes.SHOWEX_TIMINGERROR = 251;
 codes.BCI_ABORT = 252;  % use to indicate that BCI computer is not working
